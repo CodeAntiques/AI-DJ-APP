77903e59f8c39cce43f3fc4819aa8ab0a93631d6
Hanish565 <74765868+Hanish565@users.noreply.github.com>
2021-11-02 15:51:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 song="";
 
-var left_wrist_x,left_wrist_y,right_wrist_x,right_wrist_y;
+var left_wrist_x=0,left_wrist_y=0,right_wrist_x=0,right_wrist_y=0;
 
-
+var score_left_wrist=0,score_right_wrist=0;
 
 
 function preload(){
@@ -27,6 +27,9 @@ function setup(){
 
    fill("red");
    stroke("blue");
+
+   if (score_left_wrist>0.2) {
+       
    circle(left_wrist_x,left_wrist_y,20);
    number=Number(left_wrist_y);
    decimal=floor(number);
@@ -35,6 +38,43 @@ function setup(){
    document.getElementById("volume").innerHTML="Volume:" + volume;
    song.setVolume(volume);
 }
+
+
+  fill("green");
+  stroke("yellow");
+
+  if (score_right_wrist>0.2) {
+      
+  circle(right_wrist_x,right_wrist_y,20);
+
+   if (right_wrist_y >0 && right_wrist_y <100) {
+       song.rate(0.5);
+       document.getElementById("speed").innerHTML="Speed:0.5";
+
+   } else if(right_wrist_y >100 && right_wrist_y <200){
+       song.rate(1);
+       document.getElementById("speed").innerHTML="Speed:1";
+
+   }
+   else if(right_wrist_y >200 && right_wrist_y <200){
+    song.rate(1.5);
+    document.getElementById("speed").innerHTML="Speed:1.5";
+
+   }
+else if(right_wrist_y >300 && right_wrist_y <400){
+    song.rate(2);
+    document.getElementById("speed").innerHTML="Speed:2";
+
+   } 
+   else if(right_wrist_y >400 && right_wrist_y <600){
+    song.rate(2.5);
+    document.getElementById("speed").innerHTML="Speed:2.5";
+
+   }
+}
+
+}
+
 
 function play(){
     song.play();
@@ -53,6 +93,9 @@ function gotPoses(results){
         left_wrist_y=results[0].pose.leftWrist.y;
         right_wrist_x=results[0].pose.rightWrist.x;
         right_wrist_y=results[0].pose.rightWrist.y;
+     score_left_wrist=results[0].pose.keypoints[9].score;
+     score_right_wrist=results[0].pose.keypoints[10].score;
+
      console.log("leftWrist"+left_wrist_x);
      console.log("leftWrist"+left_wrist_y);
      console.log("rightWrist"+right_wrist_x);
@@ -60,6 +103,9 @@ function gotPoses(results){
 
     }
 }
+
+
+
 
 
 
